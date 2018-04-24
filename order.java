a3470722f5b3d03073533d98dd8f0a67adcabdd8
SalehALShamlan <38696105+i9aLe7esh@users.noreply.github.com>
2018-04-24 22:59:17 +0300

    Add files via upload

---
+++ order.java
@@ -0,0 +1,27 @@
+
+public class order {
+	private String type;
+	private int quantity;
+	  
+	
+	public order() {
+		type=null;
+		quantity=0;
+	}
+	public order(String x, int y) {
+		type=x;
+		quantity=y;
+	}
+	public void setType(String x) {
+		type=x;
+	}
+	public void setQuantity(int y) {
+		quantity=y;
+	}
+	public String getType() {
+		return type;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+}
